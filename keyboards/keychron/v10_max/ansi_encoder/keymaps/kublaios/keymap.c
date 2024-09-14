a9113c43492572307f65efd2aab7943332ba6d04
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-09-14 10:22:54 +0200

    Add mod-tap (tap dance) for caps lock -> ctrl/esc alongside other stuff.

--- keyboards/keychron/v10_max/ansi_encoder/keymaps/kublaios/keymap.c
+++ keyboards/keychron/v10_max/ansi_encoder/keymaps/kublaios/keymap.c
@@ -26,20 +26,20 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_89(
-        KC_MUTE,    KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,   RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_INS,             KC_DEL,
-        MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MC_3,       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
-        MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        MC_5,       KC_LCTL,  KC_LOPTN,           KC_LCMMD, KC_SPC,  MO(MAC_FN),                     KC_SPC,             KC_RCMMD,                     KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_MUTE,   KC_ESC,           KC_BRID,    KC_BRIU,  KC_F3,         KC_F4,         RGB_VAD,   RGB_VAI,  KC_MPRV,   KC_MPLY,       KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  MO(MAC_FN),         KC_DEL,
+        MC_1,      KC_GRV,           KC_1,       KC_2,     KC_3,          KC_4,          KC_5,      KC_6,     KC_7,      KC_8,          KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
+        MC_2,      KC_TAB,           KC_Q,       KC_W,     KC_E,          KC_R,          KC_T,      KC_Y,     KC_U,      KC_I,          KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+        MC_3,      LCTL_T(KC_ESC),   KC_A,       KC_S,     KC_D,          KC_F,          KC_G,      KC_H,     KC_J,      KC_K,          KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
+        MC_4,      KC_LSFT,                      KC_Z,     KC_X,          KC_C,          KC_V,      KC_B,     KC_BSPC,   KC_N,          KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        KC_LCTL,   KC_LCTL,          MC_5,                 KC_LOPTN,      LT(1,KC_SPC),  KC_LCMMD,                       LT(1,KC_SPC),            KC_ENT,                       KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_89(
-        RGB_TOG,    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
-        _______,    _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,    RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,    _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,   _______,  _______,  _______,  _______,  _______,  _______,            _______,            KC_END,
-        _______,    _______,            _______,  _______,  _______,  _______,   BAT_LVL,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
-        _______,    _______,  _______,            _______,  _______,  _______,                       _______,            _______,                      _______,  _______,  _______),
+        RGB_TOG,    _______,         KC_F1,      KC_F2,    KC_F3,          KC_F4,        KC_F5,     KC_F6,    KC_F7,    KC_F8,          KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
+        MC_2,       _______,         MC_6,       MC_7,     _______,        _______,      _______,   _______,  _______,  _______,        _______,  _______,  _______,  _______,  _______,            _______,
+        _______,    RGB_TOG,         RGB_MOD,    RGB_VAI,  RGB_HUI,        MC_8,         RGB_SPI,   _______,  _______,  _______,        _______,  _______,  _______,  _______,  _______,            _______,
+        MC_15,      _______,         RGB_RMOD,   RGB_VAD,  MC_9,           MC_10,        MC_11,     KC_LEFT,  KC_DOWN,  KC_UP,          KC_RGHT,  _______,  _______,            _______,            KC_END,
+        _______,    _______,                     _______,  _______,        _______,      _______,   MC_12,    MC_13,    NK_TOGG,        _______,  _______,  _______,  _______,  _______,  _______,
+        MC_0,       _______,         MC_14,                _______,        _______,      _______,                       _______,                  _______,                      _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_ansi_89(
         KC_MUTE,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
