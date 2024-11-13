33540601079be4cef23c47aea6bb10b67678cc1c
Kubilay Erdogan <1839554+kublaios@users.noreply.github.com>
2024-11-13 20:32:30 +0100

    Eliminate the button between spaces.

--- keyboards/keychron/v10_max/ansi_encoder/keymaps/kublaios/keymap.c
+++ keyboards/keychron/v10_max/ansi_encoder/keymaps/kublaios/keymap.c
@@ -37,12 +37,12 @@ enum {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_89(
-        KC_MUTE,    KC_ESC,          KC_BRID,    KC_BRIU,  KC_F3,         KC_F4,         RGB_VAD,   RGB_VAI,  KC_MPRV,   KC_MPLY,       KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  MO(MAC_FN),         KC_DEL,
+        KC_MUTE,    KC_ESC,          KC_BRID,    KC_BRIU,  KC_F3,         KC_F4,         RGB_VAD,   RGB_VAI,  KC_MPRV,   KC_MPLY,       KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  MC_8,               KC_DEL,
         KC_F15,     KC_GRV,          KC_1,       KC_2,     KC_3,          KC_4,          KC_5,      KC_6,     KC_7,      KC_8,          KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         MC_4,       KC_TAB,          KC_Q,       KC_W,     KC_E,          KC_R,          KC_T,      KC_Y,     KC_U,      KC_I,          KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
         KC_F19,     LCTL_T(KC_ESC),  KC_A,       KC_S,     KC_D,          KC_F,          KC_G,      KC_H,     KC_J,      KC_K,          KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
         KC_F22,     KC_LSFT,                     KC_Z,     KC_X,          KC_C,          KC_V,      KC_B,     KC_B,      KC_N,          KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
-        KC_F23,     KC_LCTL,         KC_LOPTN,             KC_LCMMD,      LT(1,KC_SPC),  MO(MAC_FN),                     LT(2,KC_SPC),            KC_ENT,                       KC_LEFT,  KC_DOWN,  KC_RGHT),
+        KC_F23,     KC_LCTL,         KC_LOPTN,             KC_LCMMD,      LT(1,KC_SPC),  _______,                        LT(2,KC_SPC),            KC_ENT,                       KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_ansi_89(
         RGB_TOG,    _______,         BT_HST1,    BT_HST2,  BT_HST3,        P2P4G,        KC_F5,     KC_F6,    KC_F7,    KC_F8,          KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
